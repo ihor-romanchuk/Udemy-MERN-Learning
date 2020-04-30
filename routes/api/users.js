59606fec6b4247a2b34e779919e823bda4f53e57
iro <iro@e-Boks.dk>
2020-04-30 18:43:28 +0300

    Added user authentication and login route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const { check, validationResult } = require('express-validator/check');
+const {check, validationResult} = require('express-validator/check');
 
 const User = require('../../models/User');
 
@@ -16,21 +16,21 @@ router.post(
     [
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6}),
     ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({errors: errors.array()});
         }
 
-        const { name, email, password } = req.body;
+        const {name, email, password} = req.body;
 
         try {
-            let user = await User.findOne({ email });
+            let user = await User.findOne({email});
 
             if (user) {
-                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+                return res.status(400).json({errors: [{msg: 'User already exists'}]});
             }
 
             const avatar = gravatar.url(email, {
@@ -57,9 +57,9 @@ router.post(
                 },
             };
 
-            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+            jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000}, (err, token) => {
                 if (err) throw err;
-                res.json({ token });
+                res.json({token});
             });
         } catch (err) {
             console.error(err.message);
